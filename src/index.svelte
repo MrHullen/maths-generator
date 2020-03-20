820ebc37c3820f5d5ee47a9587a10e6cd59e0b3f
Mr. Hullen <d.hullen@rangiruru.school.nz>
2020-03-20 15:08:48 +1300

    Update index.svelte

--- src/index.svelte
+++ src/index.svelte
@@ -1,18 +1,30 @@
 <script>
   let min = 0;
   let max = 10;
+  let operator = "+"
 
   let questions = []
 
   const createQuestions = () => {
+    questions = [];
     for (let i = 0; i < 10; i++) {
       let num1 = Math.floor((Math.random() * max) + min)
       let num2 = Math.floor((Math.random() * max) + min)
-      let question = {
-        first: ` ${num1} + ${num2} = `,
-        second: ` ${num1} - ${num2} = `,
-        third: `-${num1} + ${num2} = `,
-        fourth: `-${num1} - ${num2} = `
+      let question
+      if (operator === "+") {
+        question = {
+                first: ` ${num1} + ${num2} = `,
+                second: ` ${num1} - ${num2} = `,
+                third: `-${num1} + ${num2} = `,
+                fourth: `-${num1} - ${num2} = `
+              }
+      } else if (operator === "*") {
+        question = {
+                first: ` ${num1} x ${num2} = `,
+                second: ` ${num1} ÷ ${num2} = `,
+                third: `-${num1} x ${num2} = `,
+                fourth: `-${num1} ÷ ${num2} = `
+              }
       }
       questions = [...questions, question]
     }
@@ -30,15 +42,32 @@
 
 <h1 class="title">Maths Worksheet Generator</h1>
 
-<label class="label">
-  Min:
-  <input type="number bind:value={min}">
-</label>
+<div>
+  <div>
+    <label class="label">
+      Min:
+      <input type="number" bind:value={min}>
+    </label>
 
-<label class="label">
-  Max:
-  <input type="number bind:value={max}">
-</label>
+    <label class="label">
+      Max:
+      <input type="number" bind:value={max}>
+    </label>
+  </div>
+
+  <div>
+    <label class="label">
+      Add/Subtract:
+      <input type="radio" bind:group={operator} value={"+"}>
+    </label>
+
+    <label class="label">
+      Multiply/Divide:
+      <input type="radio" bind:group={operator} value={"*"}>
+    </label>
+  </div>
+</div>
+
 
 <button class="button" on:click={createQuestions}>Generate</button>
 
